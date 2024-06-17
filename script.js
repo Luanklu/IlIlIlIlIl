cbdb098288317bcbd58ac68339ff87412d64ce1c
Luanklu <111881070+Luanklu@users.noreply.github.com>
2024-06-17 10:44:20 -0300

    1:39

--- script.js
+++ script.js
@@ -6,25 +6,38 @@ const textoResultado = document.querySelector(".texto-resultado");
 
 const perguntas = [
     {
-        enunciado: "Pergunta 1",
+        enunciado: "Assim que saiu da escola vc se depara com uma nueva tecnologia, um chat que consegue responder todas as dúvidas que uma pessoa pode ter, ele também gera imagens e aúdios hyper-realistas. Qual seueu penzamento?",
+        alternativas: [
+            "Isso é assustador!",
+            "Isso é maravilhoso!"
+        ]
+    },
+    {
+        enunciado: "Com a descoberta desta teecnologia, chamada Inteligência Artificial (IA), uma professora de tecnologia decidiu fazer uma sequễncia de aulas sobre ela IA. No fim de uma aula ela pede que vc escreva um trabalho sobre o uso de tecnologia em sala de aula. Qual atitude vc toma?",
         alternativas: [
             "Alternativa 1",
             "Alternativa 2"
         ]
     },
     {
-        enunciado: "Pergunta 1",
+        enunciado: "Pergunta 2",
         alternativas: [
             "Alternativa 1",
             "Alternativa 2"
         ]
-    },
+    },         
     {
-        enunciado: "Pergunta 1",
+        enunciado: "Pergunta 2",
         alternativas: [
             "Alternativa 1",
             "Alternativa 2"
         ]
-    }         
-
+    }, 
+    {
+        enunciado: "Pergunta 2",
+        alternativas: [
+            "Alternativa 1",
+            "Alternativa 2"
+        ]
+    }     
 ]; 
